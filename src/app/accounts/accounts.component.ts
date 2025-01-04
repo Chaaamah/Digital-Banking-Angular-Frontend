ad57011a51a5a3e06de56f0726b95b1e8bd15be1
Chaima Agoumi <chaimaeagoumi162@gmail.com>
2025-01-05 00:43:20 +0100

    Security commit

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -3,6 +3,7 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 import {AccountsService} from '../services/accounts.service';
 import {catchError, Observable, throwError} from 'rxjs';
 import {AccountDetails} from '../model/account.model';
+import {AuthService} from '../services/auth.service';
 
 @Component({
   selector: 'app-accounts',
@@ -19,7 +20,7 @@ export class AccountsComponent implements OnInit{
   operationFormGroup! : FormGroup;
   errMsg! : string;
 
-  constructor(private fb : FormBuilder, private accountService: AccountsService ) {
+  constructor(private fb : FormBuilder, private accountService: AccountsService, public authService : AuthService) {
   }
 
   ngOnInit(): void {
